cfad84b5fba4967b452548c0b069be877955b897
Adam Baranczyk <baranczykadam@gmail.com>
2021-05-20 13:33:29 +0200

    factorial + fibonacci

--- src/packageDemo/Series.java
+++ src/packageDemo/Series.java
@@ -1,4 +1,4 @@
-package PackageDemo;
+package packageDemo;
 
 import java.util.Arrays;
 
@@ -8,6 +8,8 @@ public class Series {
 	// write your code here
         System.out.println(nSum(2,5,2));
         System.out.println(nSum(0));
+        System.out.println(factorial(5));
+        System.out.println("fibonacci: "+ fibonacci(3));
     }
     public static int nSum(int ...n){
         if(n.length == 1)
@@ -25,6 +27,24 @@ public class Series {
         else {
            return n + nSum(n-1);
         }
+    }
+    public static int factorial(int n){
+        if(n == 0)
+            return 0;
+        else if(n == 1)
+            return 1;
+        else
+            return n * factorial(n-1);
+
+    }
+
+    public static int fibonacci(int n){
+        if(n == 0)
+            return 0;
+        else if(n == 1)
+            return 1;
+        else
+            return fibonacci(n-1) + fibonacci(n-2);
     }
 
 }
