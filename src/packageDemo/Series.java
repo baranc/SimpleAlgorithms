99aec1454b9236d10d39c71db228b4b924edf299
baranc <baranczykadam@gmail.com>
2021-05-20 13:54:57 +0200

    Update Series.java

--- src/packageDemo/Series.java
+++ src/packageDemo/Series.java
@@ -23,8 +23,6 @@ public class Series {
     }
     public static int factorial(int n){
         if(n == 0)
-            return 0;
-        else if(n == 1)
             return 1;
         else
             return n * factorial(n-1);
